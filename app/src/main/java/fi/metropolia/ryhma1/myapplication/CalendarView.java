035d854f24080b96b1d48492fb6b85b96bc178a7
ChrisuTheWeak <christian.olkkonen@metropolia.fi>
2022-10-03 11:18:01 +0300

    Added New AsetusView, made Menu Item, back to manin activity Also empty xml layouts fot asetus and menu

--- app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarView.java
+++ app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarView.java
@@ -10,5 +10,7 @@ public class CalendarView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_view);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/fi/metropolia/ryhma1/myapplication/MainActivity.java
+++ app/src/main/java/fi/metropolia/ryhma1/myapplication/MainActivity.java
@@ -6,19 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private DrinkCounter drinks = new DrinkCounter();
     private TextView txtDrinks;
+    private Object Menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         txtDrinks = findViewById(R.id.txtDrinkCounter);
         txtDrinks.setText(Integer.toString(drinks.getCount()));
 
+
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
     public void openCalendar(View view) {
         Intent intent = new Intent(this, CalendarView.class);
 
@@ -32,4 +47,15 @@ public class MainActivity extends AppCompatActivity {
         drinks.minus();
         txtDrinks.setText(Integer.toString(drinks.getCount()));
     }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.actionSettings:
+                Intent intentMain = new Intent(MainActivity.this ,
+                    AsetusView.class);
+                MainActivity.this.startActivity(intentMain);
+
+        }
+        return false;
+    }
+
 }
