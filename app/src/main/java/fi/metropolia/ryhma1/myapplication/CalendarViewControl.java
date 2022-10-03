bf1e58a98af57f7150024e393bfad9927cc5f6a7
AksuMetroMaster <114567225+AksuMetroMaster@users.noreply.github.com>
2022-10-03 11:51:03 +0300

    Kalenteri palauttaa päivän

--- app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarViewControl.java
+++ app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarViewControl.java
@@ -1,16 +1,27 @@
 package fi.metropolia.ryhma1.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class CalendarViewControl extends AppCompatActivity {
-
+private CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_view);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Toast.makeText(getBaseContext(), "Selected date "+dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
