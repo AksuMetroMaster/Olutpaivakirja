1ee0f69660ce96e4aa3c3860b3f20d401b49e62a
janin <janina.risu@metropolia.fi>
2022-10-04 13:20:22 +0300

    tein toimivat tabit

--- app/src/main/java/fi/metropolia/ryhma1/myapplication/MainActivity.java
+++ app/src/main/java/fi/metropolia/ryhma1/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,21 +19,32 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtDrinks;
     private Object Menu;
     private TabLayout tabLayout;
+    private Intent intentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         tabLayout=(TabLayout) findViewById(R.id.momTab);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Log.d("bals", String.valueOf(tab));
 
+                if (tab.getPosition() == 0) {
+                    intentMain = new Intent(MainActivity.this, MainActivity.class);
 
+                }
+                if (tab.getPosition() == 1) {
+                    intentMain = new Intent(MainActivity.this,
+                            CalendarViewControl.class);
+                    Log.e("TEST", "Painettiin toista");
+                    MainActivity.this.startActivity(intentMain);
+                }
             }
+
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
