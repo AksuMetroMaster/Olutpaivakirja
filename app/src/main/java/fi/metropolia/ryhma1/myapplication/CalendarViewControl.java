10067d55540a556eeaf6939a05f9193e29816513
janin <janina.risu@metropolia.fi>
2022-10-05 11:51:58 +0300

    Lisätty tabit kalenteri aktiviteettiin, lisätty toiminnallisuus tabi valitsee aina tab1 ja kun siirtyy takaisin, lisätty strings.xml.ään main page ja default value front page.

--- app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarViewControl.java
+++ app/src/main/java/fi/metropolia/ryhma1/myapplication/CalendarViewControl.java
@@ -3,12 +3,19 @@ package fi.metropolia.ryhma1.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class CalendarViewControl extends AppCompatActivity {
 private CalendarView calendarView;
+private TabLayout tabLayout;
+private Intent intentMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,38 @@ private CalendarView calendarView;
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
                 Toast.makeText(getBaseContext(), "Selected date "+dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        tabLayout=(TabLayout) findViewById(R.id.mainPage);
+        TabLayout.Tab tab = tabLayout.getTabAt(1);
+        tab.select();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == 1) {
+                    intentMain = new Intent(CalendarViewControl.this,
+                            CalendarViewControl.class);
+
+
+                }
+                if (tab.getPosition() == 0) {
+                    intentMain = new Intent(CalendarViewControl.this, MainActivity.class);
+                    Log.e("TEST", "Painettiin toista");
+                    CalendarViewControl.this.startActivity(intentMain);
+                }
+            }
+
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
     }
